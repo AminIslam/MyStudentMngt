4eb3855ec1fc6df0a609407a8dfbf976ee4f79bd
AminIslam <aminulislamcse@gmail.com>
2017-03-17 12:39:31 +0600

    Add TestGit.cs

---
+++ StudentLayerApp/UI/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace StudentLayerApp.UI
+{
+    partial class StudentInfoUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.studentListView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtRegNo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtSearchName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // studentListView
+            // 
+            this.studentListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.studentListView.Location = new System.Drawing.Point(40, 278);
+            this.studentListView.MultiSelect = false;
+            this.studentListView.Name = "studentListView";
+            this.studentListView.Size = new System.Drawing.Size(443, 165);
+            this.studentListView.TabIndex = 16;
+            this.studentListView.UseCompatibleStateImageBehavior = false;
+            this.studentListView.View = System.Windows.Forms.View.Details;
+            this.studentListView.DoubleClick += new System.EventHandler(this.studentListView_DoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ID";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Reg No";
+            this.columnHeader2.Width = 134;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Name";
+            this.columnHeader3.Width = 89;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Address";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(109, 217);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 15;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(109, 119);
+            this.txtAddress.Multiline = true;
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(203, 77);
+            this.txtAddress.TabIndex = 14;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(109, 66);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(203, 20);
+            this.txtName.TabIndex = 13;
+            // 
+            // txtRegNo
+            // 
+            this.txtRegNo.Location = new System.Drawing.Point(109, 15);
+            this.txtRegNo.Name = "txtRegNo";
+            this.txtRegNo.Size = new System.Drawing.Size(203, 20);
+            this.txtRegNo.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Address";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Reg No";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(408, 236);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 17;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtSearchName
+            // 
+            this.txtSearchName.Location = new System.Drawing.Point(271, 238);
+            this.txtSearchName.Name = "txtSearchName";
+            this.txtSearchName.Size = new System.Drawing.Size(131, 20);
+            this.txtSearchName.TabIndex = 18;
+            this.txtSearchName.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtSearchName_KeyDown);
+            // 
+            // StudentInfoUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 458);
+            this.Controls.Add(this.txtSearchName);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.studentListView);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtRegNo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "StudentInfoUI";
+            this.Text = "Student UI";
+            this.Load += new System.EventHandler(this.StudentInfoUI_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView studentListView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtRegNo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtSearchName;
+    }
+}
---
+++ StudentLayerApp/UI/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using StudentLayerApp.MODEL;
+using StudentLayerApp.BLL;
+
+namespace StudentLayerApp.UI
+{
+    public partial class StudentInfoUI : Form
+    {
+        StudentManager objStudentManager = new StudentManager();
+        public StudentInfoUI()
+        {
+            InitializeComponent();
+        }
+
+        private void StudentInfoUI_Load(object sender, EventArgs e)
+        {
+           
+            //LoadListView();
+        }
+
+        private void LoadListView()
+        {
+            studentListView.Items.Clear();
+            List<MyStudent> students = objStudentManager.GetAllStudents();
+            foreach (MyStudent stu in students)
+            {
+                ListViewItem item = new ListViewItem(stu.Id.ToString());
+                item.SubItems.Add(stu.regNo);
+                item.SubItems.Add(stu.name);
+                item.SubItems.Add(stu.adress);
+                studentListView.Items.Add(item);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            MyStudent student = new MyStudent();
+            student.regNo = txtRegNo.Text;
+            if (objStudentManager.isRegNoExists(student.regNo))
+            {
+                MessageBox.Show("Reg No Already Exists !");
+                return;
+            }
+            student.name = txtName.Text;
+            student.adress = txtAddress.Text;
+
+
+
+            int rowAffected = objStudentManager.InsertStudent(student);
+
+            if (rowAffected > 0)
+            {
+                MessageBox.Show("Insert Successfull !");
+                LoadListView();
+                ClearTextBoxes();
+            }
+            else
+            {
+                MessageBox.Show("Operation Failed !");
+            }
+        }
+
+        private void ClearTextBoxes()
+        {
+            txtRegNo.Clear();
+            txtName.Clear();
+            txtAddress.Clear();
+        }
+
+        private void studentListView_DoubleClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            
+            if (!txtSearchName.Text.All(abc => char.IsNumber(abc)))
+            {                
+                studentListView.Items.Clear();
+                List<MyStudent> students = objStudentManager.GetStudentsByName(txtSearchName.Text);
+                foreach (MyStudent stu in students)
+                {
+                    ListViewItem item = new ListViewItem(stu.Id.ToString());
+                    item.SubItems.Add(stu.regNo);
+                    item.SubItems.Add(stu.name);
+                    item.SubItems.Add(stu.adress);
+                    studentListView.Items.Add(item);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Avoid integer number !");
+            }
+        }
+
+        private void txtSearchName_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (!txtSearchName.Text.All(abc => char.IsNumber(abc)))
+                {
+                    studentListView.Items.Clear();
+                    List<MyStudent> students = objStudentManager.GetStudentsByName(txtSearchName.Text);
+                    foreach (MyStudent stu in students)
+                    {
+                        ListViewItem item = new ListViewItem(stu.Id.ToString());
+                        item.SubItems.Add(stu.regNo);
+                        item.SubItems.Add(stu.name);
+                        item.SubItems.Add(stu.adress);
+                        studentListView.Items.Add(item);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Avoid integer number !");
+                }
+            }
+        }
+    }
+}
